8e9422081a09c37b873dc6e7faa09020a948bb26
philb56 <phil.bugden@outlook.com>
2018-01-16 17:18:16 +0000

    minor refactoring of Entering Players

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,11 +1,11 @@
 feature 'Testing infrastructure' do
-  
+
   scenario 'It renders the users names' do
     visit('/')
     fill_in 'player1', with: 'Ellie'
     fill_in 'player2', with: 'Phil'
     click_button 'Submit'
-    expect(page).to have_text 'Ellie and Phil'
+    expect(page).to have_content 'Ellie vs Phil'
   end
 
 end
