487cd27eb668df733cec65632353abffd2a3747f
Ellie <elliewem@gmail.com>
2018-01-17 14:02:59 +0000

    Adds webhelpers method to rspec files

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'see Player 2\'s hit points' do
-    visit('/')
-    fill_in 'player1', with: 'Ellie'
-    fill_in 'player2', with: 'Phil'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Phil: 100HP'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ ENV['RACK_ENV'] = 'test'
 # require our Sinatra app file
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
+require_relative './features/web_helpers.rb'
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
